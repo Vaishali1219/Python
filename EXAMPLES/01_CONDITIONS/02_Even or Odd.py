d30fe06e57f07538537eaee099c0c216a4c1b5d0
Vaishali1219 <46643151+Vaishali1219@users.noreply.github.com>
2019-02-09 19:23:51 +0530

    Create 02_Even or Odd.py

---
+++ EXAMPLES/01_CONDITIONS/02_Even or Odd.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  9 16:32:07 2019
+
+@author: vaish
+"""
+
+num = input('Enter the number: ')
+num = int(num)
+if num % 2 == 0:
+    print("even")
+else:
+    print("odd")
