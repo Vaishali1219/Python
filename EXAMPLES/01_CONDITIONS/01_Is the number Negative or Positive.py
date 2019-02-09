680c111150d1255466e812d84ac82ffb37b69ffa
Vaishali1219 <46643151+Vaishali1219@users.noreply.github.com>
2019-02-09 19:23:53 +0530

    Create 01_Is the number Negative or Positive.py

---
+++ EXAMPLES/01_CONDITIONS/01_Is the number Negative or Positive.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  9 16:28:07 2019
+
+@author: vaish
+"""
+
+num = input('Enter the number: ')
+num = int(num)
+if num<0:
+    print('negative')
+else:
+    print('positive')
