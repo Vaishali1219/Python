9d90509d188b8e35cf75d7b6a34238e3d0c9af04
Vaishali1219 <46643151+Vaishali1219@users.noreply.github.com>
2019-02-10 21:36:21 +0530

    Create 05_MULTIPLIACTION TABLE USING FOR LOOP.py

---
+++ EXAMPLES/03_CYCLES/05_MULTIPLIACTION TABLE USING FOR LOOP.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 10 12:54:18 2019
+
+@author: vaish
+"""
+
+for i in range(1, 11):
+    for j in range(1, 11):
+        print("%4d" % (i * j), end='')
+    print()
+    
+"""A Multiplication Table using For Loop"""
