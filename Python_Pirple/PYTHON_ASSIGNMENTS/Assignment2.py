ea5153d9495a04a629938e5fa9d0fa618eda9775
Vaishali1219 <46643151+Vaishali1219@users.noreply.github.com>
2019-03-04 13:51:33 +0530

    Create Assignment2.py

---
+++ Python_Pirple/PYTHON_ASSIGNMENTS/Assignment2.py
@@ -0,0 +1,22 @@
+Title = "DESCRIPTION OF MY FAVOURITE SONG"
+song, FemalePlayBack, Label = "Yedho Ninaikiren", "Manjari", "Eros-Ayangaran"
+def movie():
+ movie = "Thalainagaram"
+ return movie;
+def MalePlayBack():
+ MalePlayBack = "Devan Ekambaram"
+ return MalePlayBack;
+#FemalePlayBack = "Manjari"
+#Label = "Eros-Ayangaran"
+def ReleaseYear():
+   ReleaseYear = 2015
+   if ReleaseYear < 2018:
+    return True;
+Duration = 5.283
+print(Title)
+print("The song " + song + " is from movie " ,movie() , " by " , MalePlayBack() , " & " + FemalePlayBack)
+print("The movie was released before 2018", ReleaseYear())
+print("The song is of duration: ", Duration)
+print("A creation of " + Label)
+"""For More details of the song type Talainagaram tamil movie in google:P"""
+ 
