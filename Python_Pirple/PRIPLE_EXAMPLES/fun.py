b3e2ddbe984c97eca1928c06f2865e63cc678fdb
Vaishali1219 <46643151+Vaishali1219@users.noreply.github.com>
2019-03-04 13:50:49 +0530

    Create fun.py

---
+++ Python_Pirple/PRIPLE_EXAMPLES/fun.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 14 21:42:17 2019
+
+@author: vaish
+"""
+
+def addOne(Number):
+    output = Number + 1
+    return output
+var = 0
+print(var)
+var2 = addOne(var)
+var3 = addOne(var2)
+print(var2)
+print(var3)
+var4 = addOne(5.5 + 3.8)
+print(var4)
+
+    
