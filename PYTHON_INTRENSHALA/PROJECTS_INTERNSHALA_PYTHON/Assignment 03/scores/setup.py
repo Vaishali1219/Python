90a53a36b89a42e75a441512062e75fc01759d1a
Vaishali1219 <46643151+Vaishali1219@users.noreply.github.com>
2019-04-03 22:32:00 +0530

    Create setup.py

---
+++ PYTHON_INTRENSHALA/PROJECTS_INTERNSHALA_PYTHON/Assignment 03/scores/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 22:56:51 2019
+
+@author: vaish
+"""
+
+from setuptools import setup
+
+setup(name='scores', version='1.0', description='A package to calculate the scores of Players',
+url='#',
+      author='vaishali',
+author_email='vaishsvs12@gmail.com',
+      license='VTU',
+      packages=['scores'],
+zip_safe=False)
