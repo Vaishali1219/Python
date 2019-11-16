eeae17d7919e10a43ab4b166bfe5a9e1c7ef6c33
Vaishali1219 <46643151+Vaishali1219@users.noreply.github.com>
2019-11-16 21:39:56 +0530

    Create 01_TECHGIG_CODE_CHALLENGE_SEMI_FINAL_LOGIC_2.py

---
+++ 01_TECHGIG_CODE_CHALLENGE_SEMI_FINAL_LOGIC_2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 29 21:17:58 2019
+
+@author: vaish
+"""
+
+phases = int(input("Phases: ")) ##3 
+states = int(input("States: ")) ## 2
+Wins_Phases = int(input("Wins_Phases: ")) ## <= states ## 2 1 0
+Wins_States = int(input("Wins_States: ")) ## <= phases ## 1 2
+
+##Matrix = {}
+
+
